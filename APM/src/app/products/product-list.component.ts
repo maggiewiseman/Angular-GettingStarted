900427b512eb595e4f1034fdc7eddf37b1411be8
Marguerite Wiseman <margueritewiseman@Marguerites-MacBook-Air.local>
2017-10-07 14:30:44 -0400

    got filtered product list working

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -8,11 +8,27 @@ import { IProduct } from './product';
 })
 
 export class ProductListComponent implements OnInit {
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'rake';
+    }
+
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'rake';
+    _listFilter: string;
+    get listFilter(): string {
+        console.log('getting list filter');
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        console.log('setting list filter');
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+        console.log('filtered List: ', this.filteredProducts);
+    }
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -35,12 +51,18 @@ export class ProductListComponent implements OnInit {
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
-    
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => {
+            console.log(product);
+            return product.productName.toLocaleLowerCase().indexOf(filterBy) > 0;
+        })
+    }
     toggleImage(): void {
         this.showImage = !this.showImage;
-    };
+    }
 
     ngOnInit(): void {
         console.log('in OnInit');
-    };
+    }
 }
